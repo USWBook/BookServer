a61a3248782d19d498170acd2a60ca354db48122
hoehoeabi <0203jyh00@naver.com>
2025-07-10 21:16:38 +0900

    feat : 시큐리티체인에서 사용할JWT 인증 필터

---
+++ backend/src/main/java/com/example/demo/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package com.example.demo.global.jwt;
+
+import com.example.demo.domain.user.entity.User;
+import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.global.redis.repository.RedisTokenRepository;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+    private final RedisTokenRepository redisTokenRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws ServletException, IOException{
+        String path = request.getRequestURI();
+        String authHeader = request.getHeader("Authorization");
+
+        log.info("[JWT Filter] 요청 URI: {}", path);
+        log.info("[JWT Filter] Authorization 헤더: {}", authHeader);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            log.info("[JWT Filter] 토큰 없음, 필터 체인 통과");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.replace("Bearer ", "");
+        log.info("[JWT Filter] 추출된 토큰: {}", token);
+
+        if (redisTokenRepository.isBlacklisted(token)) {
+            log.info("[JWT Filter] 블랙리스트 토큰. 요청 차단.");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            String email = jwtProvider.extractEmail(token);
+            log.info("[JWT Filter] 추출된 이메일: {}", email);
+
+            User user = userRepository.findByEmail(email).orElse(null);
+            if (user == null) {
+                log.info("[JWT Filter] 사용자 없음");
+            } else {
+                log.info("[JWT Filter] 사용자 인증 완료: {}", user.getEmail());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        user.getEmail(),null,
+                        List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+                );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        }catch (JwtException e){
+            log.error("[JWT Filter] JWT 예외: {}", e.getMessage());
+        }
+
+        chain.doFilter(request, response);
+    }
+}
