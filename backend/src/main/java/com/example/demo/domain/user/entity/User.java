786ef0b92dd2027331b5cbacaf660e0d49d268ce
hoehoeabi <0203jyh00@naver.com>
2025-07-10 21:21:31 +0900

    feat : 사용자 member에서 user로 변경. 추후 확장예정

---
+++ backend/src/main/java/com/example/demo/domain/user/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain.user.entity;
+
+import com.example.demo.domain.user.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
