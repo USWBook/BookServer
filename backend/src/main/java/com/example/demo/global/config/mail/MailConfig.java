ce49a74b7b7a5ddcb7eb4ea66ab203a447d8790f
hoehoeabi <0203jyh00@naver.com>
2025-07-22 17:56:01 +0900

    feat : 메일에서 사용할 콘피그

--- backend/src/main/java/com/example/demo/global/config/mail/MailConfig.java
+++ backend/src/main/java/com/example/demo/global/config/mail/MailConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.global.config.mail;
 
-import lombok.Value;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
