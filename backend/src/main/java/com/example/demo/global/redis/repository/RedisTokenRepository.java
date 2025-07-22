ad3a0aa9cdb7715b6c4f6cfa39f1676878917294
hoehoeabi <0203jyh00@naver.com>
2025-07-22 16:39:00 +0900

    feat : 회원가입 및 로그인 시 토큰 관리할 레디스 콘피그 이름 바꿈

--- backend/src/main/java/com/example/demo/global/redis/repository/RedisTokenRepository.java
+++ backend/src/main/java/com/example/demo/global/redis/repository/RedisTokenRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.global.redis.repository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,18 +13,23 @@ import java.time.Duration;
 @Slf4j
 public class RedisTokenRepository {
 
+    @Qualifier("authRedisTemplate")
     private final RedisTemplate<String, String> redisTemplate;
 
     private static final String REFRESH_PREFIX = "refresh:";
     private static final String BLACKLIST_PREFIX = "blacklist:";
+    private static final String EMAIL_AUTH_PREFIX = "email-auth:";
 
+    // 리프레시 토큰 저장
     public void saveRefreshToken(String email, String refreshToken, long expirationInMillis) {
         redisTemplate.opsForValue().set(
                 REFRESH_PREFIX + email,
                 refreshToken,
                 Duration.ofMillis(expirationInMillis)
         );
+        log.info("[Redis] 인증 코드 저장: {}, 유효 시간: {}ms", email, expirationInMillis);
     }
+
 
     public String getRefreshToken(String email) {
         return redisTemplate.opsForValue().get(REFRESH_PREFIX + email);
@@ -43,5 +49,13 @@ public class RedisTokenRepository {
 
     public boolean isBlacklisted(String accessToken) {
         return redisTemplate.hasKey(BLACKLIST_PREFIX + accessToken);
+    }
+
+    public String getVerificationCode(String email) {
+        return redisTemplate.opsForValue().get(EMAIL_AUTH_PREFIX + email);
+    }
+
+    public void deleteVerificationCode(String email) {
+        redisTemplate.delete(EMAIL_AUTH_PREFIX + email);
     }
 }
