edad074e5d3321a3df331d95e6930bfdec54ae34
hoehoeabi <0203jyh00@naver.com>
2025-07-25 02:46:03 +0900

    feat : 이메일 발송 실패시 예외처리 문구 바로 에러 메세지 받도록 함

--- backend/src/main/java/com/example/demo/domain/mail/exception/MessagingFailException.java
+++ backend/src/main/java/com/example/demo/domain/mail/exception/MessagingFailException.java
@@ -3,7 +3,7 @@ package com.example.demo.domain.mail.exception;
 import com.example.demo.global.exception.AuthException;
 
 public class MessagingFailException extends AuthException {
-    public MessagingFailException() {
-        super("이메일 발송에 실패했습니다", "401");
+    public MessagingFailException(String message) {
+        super(message, "500");
     }
 }
