35b50eda21733783a21e33a686d36564b8e5ec20
hoehoeabi <0203jyh00@naver.com>
2025-08-04 21:29:47 +0900

    feat: `UserDetailsService` 구현체(`CustomUserDetailsService`) 추가 -> 기존 UserRepository

---
+++ backend/src/main/java/com/example/demo/domain/user/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain.user.service;
+
+import com.example.demo.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // DB에서 이메일을 기반으로 사용자 정보를 조회합니다.
+        // User 엔티티가 UserDetails를 구현했기 때문에 바로 반환할 수 있습니다.
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 찾을 수 없습니다: " + email));
+    }
+}
