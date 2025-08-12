ae71fb29dc4e679253c1437d637788ee2f2216c4
hoehoeabi <0203jyh00@naver.com>
2025-08-12 16:18:33 +0900

    feat: 사용 안하는 메서드 지움

--- backend/src/main/java/com/example/demo/domain/auth/dto/response/TokenResponse.java
+++ backend/src/main/java/com/example/demo/domain/auth/dto/response/TokenResponse.java
@@ -3,7 +3,4 @@ package com.example.demo.domain.auth.dto.response;
 public record TokenResponse(
         String accessToken, String refreshToken
 )   {
-    public TokenResponse withoutRefreshToken() {
-    return new TokenResponse(this.accessToken, null);
-    }
 }
