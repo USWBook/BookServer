476845c4edc8730eba292149fa3ad4c84c0814db
hoehoeabi <0203jyh00@naver.com>
2025-08-12 16:19:01 +0900

    feat: UserPrincipal를 응답하도록 바뀜

--- backend/src/main/java/com/example/demo/domain/user/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/example/demo/domain/user/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.example.demo.domain.user.service;
 
+import com.example.demo.domain.user.entity.User;
 import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.global.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,8 +20,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         // DB에서 이메일을 기반으로 사용자 정보를 조회합니다.
-        // User 엔티티가 UserDetails를 구현했기 때문에 바로 반환할 수 있습니다.
-        return userRepository.findByEmail(email)
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 찾을 수 없습니다: " + email));
+
+        // 2. User 엔티티를 사용해 새로운 UserPrincipal(완제품)을 생성하여 반환합니다.
+        return new UserPrincipal(user);
     }
 }
