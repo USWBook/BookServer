09b2fc36c245e1bbdff22b5e416dcdad5f5977a3
hoehoeabi <0203jyh00@naver.com>
2025-08-15 20:02:10 +0900

    feat: 로그 지움

--- backend/src/main/java/com/example/demo/global/security/filter/LoginAuthenticationFilter.java
+++ backend/src/main/java/com/example/demo/global/security/filter/LoginAuthenticationFilter.java
@@ -17,35 +17,36 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
-//@RequiredArgsConstructor
+//@Component
+@RequiredArgsConstructor
 @Slf4j
 public class LoginAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final AuthenticationManager authenticationManager;
 
-    public LoginAuthenticationFilter(AuthenticationManager authenticationManager) {
-        super.setAuthenticationManager(authenticationManager);
-        // 기본 처리 URL은 SecurityConfig에서 setFilterProcessesUrl로 바꿔줘도 됨
-    }
+//    public LoginAuthenticationFilter(AuthenticationManager authenticationManager) {
+//        super.setAuthenticationManager(authenticationManager);
+//        // 기본 처리 URL은 SecurityConfig에서 setFilterProcessesUrl로 바꿔줘도 됨
+//    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-        log.info("@@@@@@@@@@@@@@@2로그인 필터 진입@@@@@@@@@@@@@@@22");
+
         if (!"POST".equalsIgnoreCase(request.getMethod())) {
             throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
         }
 
         try (ServletInputStream is = request.getInputStream()) {
+            ObjectMapper objectMapper = new ObjectMapper();
             LoginRequest loginRequest = objectMapper.readValue(is, LoginRequest.class);
 
             UsernamePasswordAuthenticationToken authRequest =
                     new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
 
             setDetails(request, authRequest);
-            log.info("@@@@@@@@@@@@@@@2로그인 필터 응답@@@@@@@@@@@@@@@22");
-            return this.getAuthenticationManager().authenticate(authRequest);
+
+            return authenticationManager.authenticate(authRequest);
         } catch (IOException e) {
             throw new AuthenticationServiceException("Failed to parse authentication request body", e);
         }
