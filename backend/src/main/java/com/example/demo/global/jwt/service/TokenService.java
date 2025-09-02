654ffdb16f7eb0e2232a405886c524c57b2b04bc
hoehoeabi <0203jyh00@naver.com>
2025-09-02 21:51:33 +0900

    feat: banUser에서 blacklistToken랑 중복되는 코드 있어서 blacklistToken사용하도록 바꿈

--- backend/src/main/java/com/example/demo/global/jwt/service/TokenService.java
+++ backend/src/main/java/com/example/demo/global/jwt/service/TokenService.java
@@ -89,8 +89,7 @@ public class TokenService {
     @Transactional
     public void banUser(String accessToken, String email) {
         // 1. 블랙리스트 등록
-        long expiration = jwtProvider.getTokenRemainingTime(accessToken);
-        redisTokenRepository.blacklistToken(accessToken, expiration);
+        blacklistToken(accessToken);
 
         // 2. 리프레시 토큰 삭제
         redisTokenRepository.deleteRefreshToken(email);
@@ -106,7 +105,10 @@ public class TokenService {
         return jwtProvider.getRefreshTokenExpirationInMillis();
     }
 
-    public void validateRefreshToken(String refreshToken, String email) {
+    // reissue시 이미 만료된 access토큰도 받아서 두 토큰 이메일 값도 비교해볼까 고민중
+    // 이러면 리프레쉬 토큰만 탈취한 해커가 리프레시토큰 단독으론 못뚫지 않나 생각하면서도
+    // 어차피 jwt라는게 암복호화가 쉬운거라 이메일 파싱해서 가짜access토큰 만들어버리면 그만이지 않나 싶음
+    private void validateRefreshToken(String refreshToken, String email) {
         if (!Objects.equals(jwtProvider.extractEmail(refreshToken), email)) throw new JwtInvalidSignatureException();
     }
 }
