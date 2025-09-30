870e39ba63f150a4391d89f5a004dee1e3336e3c
lotocol <cabbz987@naver.com>
2025-09-30 18:48:51 +0900

    SockJS 제거

--- backend/src/main/java/com/example/demo/global/config/chat/WebSocketConfig.java
+++ backend/src/main/java/com/example/demo/global/config/chat/WebSocketConfig.java
@@ -16,8 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
                 .addInterceptors(webSocketChatHandler)
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
+                //.withSockJS();
     }
 
     @Override
