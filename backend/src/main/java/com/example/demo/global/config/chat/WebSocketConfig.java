58c929cbb54f7bbc1377f7331ad5d0a34c395dcf
lotocol <cabbz987@naver.com>
2025-10-01 20:53:35 +0900

    웹소켓 코드 ã수정

--- backend/src/main/java/com/example/demo/global/config/chat/WebSocketConfig.java
+++ backend/src/main/java/com/example/demo/global/config/chat/WebSocketConfig.java
@@ -1,19 +1,22 @@
 package com.example.demo.global.config.chat;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketChatHandler webSocketChatHandler;  // 인터셉터 주입
-
+    private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        log.info("[WebSocket] STOMP 엔드포인트 등록: /ws-chat");
         registry.addEndpoint("/ws-chat")
                 .addInterceptors(webSocketChatHandler)
                 .setAllowedOriginPatterns("*");
