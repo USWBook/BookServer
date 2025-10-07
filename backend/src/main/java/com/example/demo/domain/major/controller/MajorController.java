16d9dd9c6c983a889d6f52900d1ee085c13602cb
hoehoeabi <0203jyh00@naver.com>
2025-10-07 21:33:50 +0900

    Feat : 스웨거 문구 추가

--- backend/src/main/java/com/example/demo/domain/major/controller/MajorController.java
+++ backend/src/main/java/com/example/demo/domain/major/controller/MajorController.java
@@ -3,6 +3,9 @@ package com.example.demo.domain.major.controller;
 import com.example.demo.domain.major.response.MajorResponse;
 import com.example.demo.domain.major.service.MajorService;
 import com.example.demo.global.response.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Majors", description = "전공관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/major")
@@ -17,6 +21,8 @@ public class MajorController {
 
     private final MajorService majorService;
 
+    @Operation(summary = "전공 리스트 보기", description = "모든 전공 이름들을 불러옵니다")
+    @ApiResponse(responseCode = "200", description = "전공리스트 불러오기 성공")
     @GetMapping("/list")
     public RsData<?> getAllMajors() {
         List<MajorResponse> majors = majorService.getAllMajors();
