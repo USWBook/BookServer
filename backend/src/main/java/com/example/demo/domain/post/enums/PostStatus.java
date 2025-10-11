a174467003a62c49bcddfa84e124dec9c8f841bf
hoehoeabi <0203jyh00@naver.com>
2025-10-11 21:38:30 +0900

    Refactor : PostStatus 변경

--- backend/src/main/java/com/example/demo/domain/post/enums/PostStatus.java
+++ backend/src/main/java/com/example/demo/domain/post/enums/PostStatus.java
@@ -1,5 +1,33 @@
 package com.example.demo.domain.post.enums;
 
+import com.example.demo.domain.post.exception.InvalidPostStatusException;
+import com.example.demo.domain.user.exception.InvalidGradeException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public enum PostStatus {
-    판매중, 판매완료;
+    PostStatus_1("판매중"),
+    PostStatus_2("판매완료");
+
+    private final String value;
+
+    PostStatus(String value) {this.value = value;}
+
+
+    @JsonCreator
+    public static com.example.demo.domain.post.enums.PostStatus fromValue(String value) {
+        return Stream.of(com.example.demo.domain.post.enums.PostStatus.values())
+                .filter(g -> Objects.equals(g.getValue(), value))
+                .findFirst()
+                .orElseThrow(() -> new InvalidPostStatusException(value + "는 유효하지 않은 상태입니다."));
+    }
+
+    // Enum을 JSON으로 변환할 때 String 값으로 나가도록 설정
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
