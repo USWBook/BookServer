acda1b487e0035eaedee936ae329c9bcb0080c27
hoehoeabi <0203jyh00@naver.com>
2025-10-13 20:43:38 +0900

    delete : 사용안해서지움

--- backend/src/main/java/com/example/demo/domain/mail/exception/VerificationCodeNotRequestedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.mail.exception;
-
-import com.example.demo.global.exception.AuthException;
-
-public class VerificationCodeNotRequestedException extends AuthException {
-    public VerificationCodeNotRequestedException() {
-        super("인증코드를 요청하지 않음" , "400" );
-    }
-}
