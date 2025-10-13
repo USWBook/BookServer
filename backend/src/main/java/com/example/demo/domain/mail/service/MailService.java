bd1f5e774fdcd0efa227dc3458a2b8615b541194
hoehoeabi <0203jyh00@naver.com>
2025-10-13 20:58:59 +0900

    REFACTOR : 1. 인증 목적에 따라 따로 저장하도록(비밀번호 초기화,회원가입) 로직 변경(추후 이메일 인증 사용처가 늘어나더라도 이넘에서 늘리기만 하면 됨) 2. 재요청은 1분뒤에 가능하도록 바꿈 3. 인증코드 검증이 성공하면 이메일 전송상태 삭제하도록 바꿈

--- backend/src/main/java/com/example/demo/domain/mail/service/MailService.java
+++ backend/src/main/java/com/example/demo/domain/mail/service/MailService.java
@@ -1,73 +1,86 @@
 package com.example.demo.domain.mail.service;
 
-import com.example.demo.domain.mail.entity.MailStatus;
+import com.example.demo.domain.mail.dto.MailRequestOrVerifyDto;
+import com.example.demo.domain.mail.enums.EmailAuthPurpose;
+import com.example.demo.domain.mail.enums.MailStatus;
+import com.example.demo.domain.mail.exception.InvalidEmailAuthPurposeException;
 import com.example.demo.domain.mail.exception.InvalidOrExpiredVerificationCodeException;
-import com.example.demo.domain.mail.exception.MessagingFailException;
-import com.example.demo.domain.mail.exception.VerificationCodeNotRequestedException;
-import com.example.demo.global.redis.repository.RedisTokenRepository;
-import jakarta.transaction.Transactional;
+import com.example.demo.domain.mail.exception.TooManyMailRequestException;
+import com.example.demo.global.redis.repository.RedisMailRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-
 
 @Service
 @RequiredArgsConstructor
 public class MailService {
 
     private final EmailSendingService emailSendingService;
-    private final RedisTokenRepository redisTokenRepository;
+    private final RedisMailRepository redisMailRepository;
 
     private static final long AUTH_CODE_EXPIRATION_MILLIS = 1800000L; // 30분
 
     @Transactional
-    public void sendVerificationCode(String email) throws Exception {
+    public void sendVerificationCode(MailRequestOrVerifyDto mailRequestOrVerifyDto) throws Exception {
         String code = createAuthCode();
-        redisTokenRepository.saveVerificationCode(email, code, AUTH_CODE_EXPIRATION_MILLIS);
+        String email = mailRequestOrVerifyDto.email();
+        EmailAuthPurpose purpose = mailRequestOrVerifyDto.purpose();
 
-        // 1. 비동기 작업 호출 직후, 상태를 PENDING으로 저장
-        redisTokenRepository.saveMailStatus(email, MailStatus.PENDING);
+        // 1분에 한번만 요청 가능하게 잠금확인
+        if (redisMailRepository.hasMailRequestLock(email, purpose)) {
+            throw new TooManyMailRequestException();
+        }
 
-        CompletableFuture<Void> emailResult = emailSendingService.sendAuthCodeEmail(email, code);
+        // 1분에 한번만 요청 가능하게 잠금
+        redisMailRepository.saveMailRequestLock(email, purpose);
+        // 인증코드 저장
+        redisMailRepository.saveVerificationCode(email, code, AUTH_CODE_EXPIRATION_MILLIS, purpose);
+        // 비동기 작업 호출하기전 상태를 PENDING으로 설정
+        redisMailRepository.saveMailStatus(email, MailStatus.PENDING, purpose);
+
+        CompletableFuture<Void> emailResult = emailSendingService.sendAuthCodeEmail(email, code,purpose.getValue());
 
         emailResult.thenAccept(voidResult -> {
-            // 작업 성공 시, 상태를 SUCCESS로 업데이트
-            redisTokenRepository.saveMailStatus(email, MailStatus.SUCCESS);
-            System.out.println(email + " 주소로 메일 발송 성공!");
+            redisMailRepository.saveMailStatus(email, MailStatus.SUCCESS, purpose);
+            System.out.println(email + " 주소로 [" + purpose + "] 목적의 메일 발송 성공!");
         });
 
         emailResult.exceptionally(ex -> {
-            //  작업 실패 시, 상태를 FAILED로 업데이트
-            redisTokenRepository.saveMailStatus(email, MailStatus.FAILED);
-            System.err.println(email + " 주소로 메일 발송 실패: " + ex.getMessage());
+            redisMailRepository.saveMailStatus(email, MailStatus.FAILED, purpose);
+            System.err.println(email + " 주소로 [" + purpose + "] 목적의 메일 발송 실패: " + ex.getMessage());
             return null;
         });
     }
 
-    //  상태 조회를 위한  메서드
-    public MailStatus getMailStatus(String email) {
-        return redisTokenRepository.getMailStatus(email);
+    public Optional<MailStatus> getMailStatus(String email, EmailAuthPurpose purpose) {
+        return redisMailRepository.getMailStatus(email, purpose);
     }
 
     @Transactional
-    public void verifyEmail(String email, String code) {
+    public void verifyEmail(String email, String code, EmailAuthPurpose purpose) {
 
         // 이메일 인증코드를 보낸 적이 없다면 예외 처리
-        if (!redisTokenRepository.existsVerificationCode(email)) {
-            throw new VerificationCodeNotRequestedException();
+        if (!redisMailRepository.existsVerificationCode(email, purpose)) {
+            throw new InvalidEmailAuthPurposeException(email);
         }
 
-        String savedCode = redisTokenRepository.getVerificationCode(email);
-        if (savedCode == null || !savedCode.equals(code)) {
+        String savedCode = redisMailRepository.getVerificationCode(email, purpose)
+                .orElseThrow(InvalidOrExpiredVerificationCodeException::new);
+
+        if (!savedCode.equals(code)) {
             throw new InvalidOrExpiredVerificationCodeException();
         }
 
         // 인증 성공: Redis에 인증된 이메일 저장 (TTL 20분 설정)
-        redisTokenRepository.setVerifiedEmail(email);  // "verified:email" -> "true"
-
-        redisTokenRepository.deleteVerificationCode(email); // 인증 완료 후 코드 삭제
+        redisMailRepository.setVerifiedEmail(email, purpose);
+        // 인증 성공: Redis에 저장된 인증코드 삭제
+        redisMailRepository.deleteVerificationCode(email, purpose);
+        // 인증 성공: Redis에 저장된 이메일 전송상태 삭제
+        redisMailRepository.deleteMailStatus(email, purpose);
     }
 
     private String createAuthCode() {
