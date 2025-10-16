867699980155ffa6e2284225d8afee8210254e76
lotocol <cabbz987@naver.com>
2025-10-16 14:04:27 +0900

    메시지 읽음 처리 추가

--- backend/src/main/java/com/example/demo/domain/chat/repository/ChatMessageRepository.java
+++ backend/src/main/java/com/example/demo/domain/chat/repository/ChatMessageRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.chat.repository;
 import com.example.demo.domain.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,5 +14,8 @@ import java.util.UUID;
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, UUID> {
     //특정 채팅방(roomId)의 모든 메시지 조회
     List<ChatMessage> findByChatRoomId(UUID roomId);
+
+    // 채팅방에서 특정 시각 이전(포함) 메시지 모두 조회
+    List<ChatMessage> findByChatRoomIdAndSentAtLessThanEqual(UUID chatRoomId, LocalDateTime lastReadAt);
 
 }
