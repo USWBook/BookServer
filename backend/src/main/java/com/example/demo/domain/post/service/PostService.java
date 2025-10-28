9e66302bb39832eabb2505d60ddbaddeb9b01159
hoehoeabi <0203jyh00@naver.com>
2025-10-28 20:20:27 +0900

    Feat : 게시물 상태를 다시 판매중으로 바꾸는 기능 추가
    이경우 기존에 성립한 거래 관계를 지움

--- backend/src/main/java/com/example/demo/domain/post/service/PostService.java
+++ backend/src/main/java/com/example/demo/domain/post/service/PostService.java
@@ -11,6 +11,8 @@ import com.example.demo.domain.post.exception.AlreadyReportException;
 import com.example.demo.domain.post.exception.CommentNotFoundException;
 import com.example.demo.domain.post.exception.CommentNotInPostException;
 import com.example.demo.domain.post.repository.PostCommentRepository;
+import com.example.demo.domain.purchase.entity.PurchaseHistory;
+import com.example.demo.domain.purchase.repository.PurchaseHistoryRepository;
 import com.example.demo.domain.report.entity.UserReport;
 import com.example.demo.domain.report.repository.UserReportRepository;
 import com.example.demo.domain.user.entity.User;
@@ -42,6 +44,7 @@ public class PostService {
     private final UserRepository userRepository;
     private final PostCommentRepository postCommentRepository;
     private final UserReportRepository userReportRepository;
+    private final PurchaseHistoryRepository purchaseHistoryRepository;
 
     // 게시글 생성
     @Transactional
@@ -193,9 +196,11 @@ public class PostService {
         Post post = postRepository.findById(postId)
                 .orElseThrow(PostNotFoundException::new);
 
+        Optional<PurchaseHistory> purchaseOpt = purchaseHistoryRepository.findByPostId(postId);
+        purchaseOpt.ifPresent(purchaseHistoryRepository::delete);
+        
         post.markAsSell();
 
-        return;
     }
 
     // 판매완료로 변경
