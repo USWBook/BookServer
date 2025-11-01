73165b08be997978049c9ca456eaec172c4a5994
sojunsik <chunsik1003@gmail.com>
2025-11-01 19:21:33 +0900

    권한,이미지경로 추가

--- backend/src/main/java/com/example/demo/global/config/S3Config.java
+++ backend/src/main/java/com/example/demo/global/config/S3Config.java
@@ -3,7 +3,9 @@ package com.example.demo.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -11,22 +13,31 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${custom.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${custom.aws.credentials.secret-key}")
-    private String secretKey;
-
     @Value("${custom.aws.s3.bucket}")
     private String bucket;
 
+    // Local/Dev 환경: AccessKey 사용
     @Bean
-    public S3Client s3Client() {
+    @Profile({"local", "dev"})
+    public S3Client s3ClientLocal(
+            @Value("${custom.aws.credentials.access-key}") String accessKey,
+            @Value("${custom.aws.credentials.secret-key}") String secretKey
+    ) {
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
         
         return S3Client.builder()
-                .region(Region.AP_NORTHEAST_2) // 서울 리전
+                .region(Region.AP_NORTHEAST_2)
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+
+    // STG/Prod 환경: IAM Role 사용
+    @Bean
+    @Profile({"stg", "prod"})
+    public S3Client s3ClientCloud() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
@@ -35,6 +46,7 @@ public class S3Config {
         return bucket;
     }
 }
+
 
 
 
